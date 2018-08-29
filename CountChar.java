bd8fc043dd43f79b738dd9bf042eb6f1f3e87dfa
Nandha kumar S <mailstonandhakumar@gmail.com>
2018-08-29 22:28:57 +0530

    Update and rename README.md to CountChar.java

---
+++ CountChar.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+    public class CountChar{
+      public static void main(String args[]){
+      //Scanner sc=new Scanner(System.in);
+      System.out.println("Enter Your Strinf...");
+      String Input=laptop is good;
+      int count=0;
+      for(int i=0;i<Input.length();i++)
+      {
+        if(Input.charAt(i)!=' '){
+        count++;
+        }
+      }
+      System.out.println("The Count is "+count);
+     }
+    } 
